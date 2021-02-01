3404ac67ef0cec38419de812600143a317dd2805
trash <73361653+galenddavis@users.noreply.github.com>
2021-02-01 10:32:15 -0500

    Set basic routes for users

--- app.js
+++ app.js
@@ -1,7 +1,20 @@
+const mongoose = require('mongoose');
+
 const express = require('express');
 const app = express();
+const db = require('./config/keys').mongoURI;
+const users = require('./routes/api/users')
+const bodyParser = require('body-parser');
+
+mongoose
+    .connect(db, { useNewUrlParser: true })
+    .then(() => console.log('Connected to MongoDB successfully'))
+    .catch(err => console.log(err));
 
 app.get('/', (req, res) => res.send('You only have one week to do this.'))
+app.use('/api/users', users)
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 const port = process.env.PORT || 5000;
 
