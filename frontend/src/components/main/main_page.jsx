dc77e0f802bd4f4adeade8ec20b44b5d0880ce62
Jose Mejia <josemejia247@gmail.com>
2021-02-04 00:18:20 -0500

    worked on animation

--- frontend/src/components/main/main_page.jsx
+++ frontend/src/components/main/main_page.jsx
@@ -5,6 +5,7 @@ class MainPage extends React.Component {
 
     render() {
         return (
+<<<<<<< HEAD
             <span></span>
         )
         // return (
@@ -21,6 +22,21 @@ class MainPage extends React.Component {
         //             <span>What will you make?</span>
         //     </div>
         // );
+=======
+            <div className='animation'>
+                <div className="container">
+                    <span className='bun'>Countless</span> 
+                    <div className="flip">
+                        <div><div>pasta</div></div>
+                        <div><div>chicken</div></div>
+                        <div><div>fish</div></div>
+                    </div>
+                    <span className='bun'>recipes</span> 
+                </div>
+                    {/* <span>What will you make?</span> */}
+            </div>
+        );
+>>>>>>> worked on animation
     }
 }
 
