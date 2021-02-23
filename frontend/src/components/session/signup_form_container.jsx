901a73278951e884ec24f0832c45cdf2dde2193a
Jose Mejia <josemejia247@gmail.com>
2021-02-23 10:07:52 -0500

    worked on user auth fixes

--- frontend/src/components/session/signup_form_container.jsx
+++ frontend/src/components/session/signup_form_container.jsx
@@ -1,16 +1,18 @@
 import { connect } from 'react-redux';
 import { signup, login, removeErrors } from '../../actions/session_actions';
 import SessionForm from './session_form'
-import { closeModal } from "../../actions/modal_actions";
+import { closeModal, openModal } from "../../actions/modal_actions";
 
 const mSTP = state => ({
     errors: Object.values(state.errors.session),
+    user: state.session.isAuthenticated,
     formType: 'Sign Up'
 })
 
 const mDTP = (dispatch) => ({
   processForm: (user) => dispatch(signup(user)),
   closeModal: () => dispatch(closeModal()),
+  openModal: () => dispatch(openModal('signup')),
   login: (user) => dispatch(login(user)),
   removeErrors: () => dispatch(removeErrors())
 });
